92f0493a798382816c502fe851e379f686a792b3
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2023-10-07 09:27:50 +0600

    bug fixed

--- make_calcultor_use_switch_case.c
+++ make_calcultor_use_switch_case.c
@@ -5,9 +5,13 @@ int main(){
     double num1, num2;
 
     printf("Enter the opeator(+,-,*,/): ");
-    scanf("%c",&opeations);
-    printf("Enter the operand num1 and num2: ",num1,num2);
-    scanf("%lf %lf",&num1,&num2);
+    scanf("%c",&opeations)
+        
+    printf("Enter the number1: ",num1);
+    scanf("%lf",&num1);
+
+    printf("Enter the number2: ",num2);
+    scanf("%lf",&num2);
 
     switch (opeations)
     {
